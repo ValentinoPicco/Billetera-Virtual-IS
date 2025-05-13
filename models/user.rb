e8a9d9dc5054c1efb7fe5e15d70e433e27a957f4
Fran Miani <127223339+FranMiani@users.noreply.github.com>
2025-05-13 20:33:38 -0300

    Update user.rb
    deleted attr_reader

--- models/user.rb
+++ models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
 
-  attr_reader :dni, :name, :surname, :email, :tel, :address #attr_accessor ??
-
   def initialize(dni, name, surname, email, tel, address)
     @dni = dni
     @name = name
