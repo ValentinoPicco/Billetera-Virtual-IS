b59a87559b75942402be87bffcbaed5f647a69bd
Valentino Picco <valentinopicco2004@gmail.com>
2025-05-13 20:12:40 -0300

    service-class

---
+++ models/service.rb
@@ -0,0 +1,9 @@
+class Service
+    attr_reader :id_service, :nom_service, :monto_mensual, :fecha_pago
+
+    def initialize(id_service, nom_service, monto_mensual, fecha_pago)
+        @id_service = id_service
+        @nom_service = nom_service
+        @monto_mensual = monto_mensual
+        @fecha_pago = fecha_pago
+    end
