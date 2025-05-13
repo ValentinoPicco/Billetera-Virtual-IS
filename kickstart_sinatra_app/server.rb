bbff9a0ee6e7a3dae4c9f1bbe243d1384b853e8a
FranMiani <mianifrancisco04@gmail.com>
2025-05-13 19:25:33 -0300

    KickStart

---
+++ kickstart_sinatra_app/server.rb
@@ -0,0 +1,25 @@
+require 'bundler/setup'
+require 'sinatra/base'
+require 'sinatra/reloader' if Sinatra::Base.environment == :development
+require 'sinatra/activerecord'
+require 'logger'
+require_relative 'models/user'
+
+class App < Sinatra::Application
+
+  configure :development do
+    enable :logging
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::DEBUG if development?
+    set :logger, logger
+
+    register Sinatra::Reloader
+    after_reload do
+      logger.info 'Reloaded!!!'
+    end
+  end
+
+  get '/' do
+    'Welcome'
+  end
+end
