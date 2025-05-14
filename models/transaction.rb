4c62376897bdc55403621f421302b475809bc82b
Valentino Picco <valentinopicco2004@gmail.com>
2025-05-14 14:04:28 -0300

    enum-transaction

--- models/transaction.rb
+++ models/transaction.rb
@@ -1,4 +1,16 @@
 class Transaction < ActiveRecord::Base
+  self.primary_key = 'num_operation'
 
+  enum type: {
+    TRASFERENCIA_RECIBIDA: 0,TRANSFERERENCIA_ENVIADA: 1,
+    TRANSFERERENCIA_ENVIADA: 1,
+    SALARIO: 2,
+    DEVOLUCION: 3,
+    DEPOSITO: 4,
+    ADELANTO_DE_SUELDO: 5,
+    PAGO_SERVICIO: 6,
+    COMPRA: 7,
+    RETIRO: 8
+  }
 
 end
