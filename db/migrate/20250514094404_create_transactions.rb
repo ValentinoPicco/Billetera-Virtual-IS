6fd4e8e253a67e0201d6f55630a9aa69ac0a892b
Valentino Picco <valentinopicco2004@gmail.com>
2025-05-14 15:03:45 -0300

    correcion-transaction-2-la-venganza

--- db/migrate/20250514094404_create_transactions.rb
+++ db/migrate/20250514094404_create_transactions.rb
@@ -1,15 +1,15 @@
 class CreateTransactions < ActiveRecord::Migration[7.0]
   def change
-    create_table :transactions, id: false, primary_key: num_operation do |t|
+    create_table :transactions, id: false do |t|
       t.integer :num_operation, null: false
       t.date :date
-      t.integer :type, default: 0, null: false # tTransaction
-      t.decimal :value, presicion: 10,  scale: 2
+      t.integer :transaction_type, default: 0, null: false
+      t.decimal :value, precision: 10, scale: 2
       t.string :reason
       t.timestamps
     end
 
-    # Declaramos id_service como clave primaria
+    # Declaramos num_operation como clave primaria
     execute "ALTER TABLE transactions ADD PRIMARY KEY (num_operation);"
   end
 end
