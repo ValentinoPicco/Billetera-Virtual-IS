fba49890a3345d8af1ee013d82e27266b0c1a63f
Valentino Picco <valentinopicco2004@gmail.com>
2025-05-17 15:36:54 -0300

    correccion-monto-service

--- db/migrate/20250513125200_create_services.rb
+++ db/migrate/20250513125200_create_services.rb
@@ -2,7 +2,7 @@ class CreateServices < ActiveRecord::Migration[7.0]
   def change
     create_table :services do |t|
       t.string :nom_service
-      t.decimal :monto_mensual, precision: 10, scale: 2
+      t.integer :monto_mensual
       t.date :fecha_pago
 
       t.timestamps
