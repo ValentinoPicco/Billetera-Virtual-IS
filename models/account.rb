fe9bc29353a6d51f0a6c4228d1dca5d28dd8ec57
Valentino Picco <valentinopicco2004@gmail.com>
2025-05-17 00:58:05 -0300

    cuentas-contacto-model

--- models/account.rb
+++ models/account.rb
@@ -1,6 +1,7 @@
 class Account < ActiveRecord::Base
-    # belongs_to :user
+    belongs_to :user
 
+    has_many :contact_account, class_name: 'Account', foreign_key: :contact_account_id
     # has_many :transaction
     #has_many :source_transacction, class name: 'transaction', foreing key transaction
 end
