e55e17a771d2d8e5218c882bc4fc4772de935892
Valentino Picco <valentinopicco2004@gmail.com>
2025-05-17 15:59:20 -0300

    correcion-version-migrate

--- db/migrate/20250514094404_create_transactions.rb
+++ db/migrate/20250514094404_create_transactions.rb
@@ -1,4 +1,4 @@
-class CreateTransactions < ActiveRecord::Migration[7.0]
+class CreateTransactions < ActiveRecord::Migration[8.0]
   def change
     create_table :transactions do |t|
       t.integer :num_operation, null: false
