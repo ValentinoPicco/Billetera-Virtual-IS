8bac73d56a31fa2e6b463c2e5b5acbd1ce990839
mateollorente <mateollorente25@gmail.com>
2025-06-19 17:08:49 -0300

    eliminacion usuario sin cuenta

--- deleteUser.rb
+++ deleteUser.rb
@@ -1,10 +1,5 @@
 require_relative 'server'
-account = Account.find_by(user_id:8)
-account&.source_transactions&.destroy_all
-account&.target_transactions&.destroy_all
-account&.destroy
-puts "Usuario eliminado correctamente"
-user = User.find_by(dni: 46)
+user = User.find_by(dni: 1234)
 if user
   user.account&.source_transactions&.destroy_all
   user.account&.target_transactions&.destroy_all
