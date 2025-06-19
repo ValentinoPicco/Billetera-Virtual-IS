6c4f0076a63231374996a77181ca9ec636d41925
Valentino Picco <valentinopicco2004@gmail.com>
2025-06-19 19:32:07 -0300

    borrando other_cards

--- models/other_card.rb
+++
@@ -1,5 +0,0 @@
-class OtherCard < Card
-  # Hereda los Callbacks, Validations y Assosiations de Card
-  # No hace falta sobreescribir los Callbacks a no ser que deban
-  # comportarse distinto en OtherCard
-end
